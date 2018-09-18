d22541ed662996a36b99674d6c3f109915efda1d
Allisa LeBeuf <al5906@gmail.com>
2018-09-18 10:51:41 -0700

    feature one complete

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
-  response.render('index', {pageTitle: 'Book App'});
+  response.render('index', {pageTitle: 'Hello World'});
 });
 
 app.listen(PORT, () => {
