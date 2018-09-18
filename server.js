61480f8cef8fb59e35e9d368d727f2d71872b73c
brinel321 <brian.c.nelson1@outlook.com>
2018-09-18 11:35:13 -0700

    setting up heroku

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
-  response.render('index', {pageTitle: 'Hello World'});
+  response.render('index', {pageTitle: 'Hello World ROcks!!!!'});
 });
 
 app.listen(PORT, () => {
